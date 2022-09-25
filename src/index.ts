da0c6e14f9769604cd8dc5c415b90aa1523c6043
Ridercogswell <ridercogswell@gmail.com>
2022-09-25 13:22:23 -0700

    add get method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('hi there');
+import { User } from "./models/User";
+
+const user = new User({ name: 'Rider', age: 24 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
