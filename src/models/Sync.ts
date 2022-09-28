0e9ad8a4b4990c16aad44803e276da6152c81564
Ridercogswell <ridercogswell@gmail.com>
2022-09-28 00:23:28 -0700

    refactor sync with generics

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,7 +1,10 @@
 import axios, { AxiosPromise } from 'axios';
-import { UserProps } from './User';
 
-export class Sync {
+interface HasId {
+  id: number;
+}
+
+export class Sync<T extends HasId> {
 
   constructor(public rootUrl: string) {}
 
@@ -9,7 +12,7 @@ export class Sync {
     return axios.get(`${this.rootUrl }/${id}`)
   }
 
-  save(data: UserProps): AxiosPromise {
+  save(data: T): AxiosPromise {
     const { id } = data
 
     if (id) {
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import { Eventing } from './Eventing'
+import { Sync } from './Sync';
  
 export interface UserProps{
   name?: string;
@@ -6,8 +7,11 @@ export interface UserProps{
   id?: number;
 }                 
 
-export class User {
-  events: Eventing = new Eventing(); // 
+const rootUrl = 'http://localhost/3000/users';
+
+export class User  {
+  public events: Eventing = new Eventing(); // hardcoded poor approach
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl) // pass in UserProps to the sync function
 
   constructor(private data: UserProps) {};
 
