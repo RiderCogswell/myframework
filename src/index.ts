9548b7a8da3e3a75311d6ae3eb9b3cf840e9792c
Ridercogswell <ridercogswell@gmail.com>
2022-10-03 15:11:11 -0700

    test not working, revisit after class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from "./models/User";
 
-const user = new User({ name: 'newwwwr', age: 234, id: 1 })
+const user = User.buildUser({ id: 1})
 
-user.on('save', () => {
+user.on('change', () => {
   console.log(user);
 })
 
-user.save();
+user.fetch();
